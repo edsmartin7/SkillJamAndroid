2466fe0c56294c9fd71a01b1445cffef168c64c2
edsmartin7 <edsmartin7@gmail.com>
2017-10-18 00:06:48 -0400

    first commit

---
+++ app/src/main/java/com/example/emartin/skilljamandroid/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.emartin.skilljamandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//color #D4D390 + white
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
